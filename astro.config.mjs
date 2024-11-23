464b01729efb407de075111fa02606c33db72214
Nichokas <nicolas.rodalv@educa.jcyl.es>
2024-11-23 14:18:28 +0100

    Fix movile error
    https://github.com/JonasKruckenberg/tauri-astro-template/pull/28

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+
 import playformCompress from "@playform/compress";
 import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: mobile ? "0.0.0.0" : false,
+    port: 4321,
+  },
   integrations: [solidJs(), playformInline(), playformCompress()]
 });
